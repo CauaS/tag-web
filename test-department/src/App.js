97000c237f6b2fbeec40d126354255804226b38a
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-07-19 13:42:43 -0300

    First Deletes

---
+++ test-department/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
